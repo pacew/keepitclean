bf891a432fe06042018ebff4599c271182815389
Pace Willisson <pace@alum.mit.edu>
2011-08-07 11:38:12 -0400

    cleanup

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,13 +1,42 @@
-var keepitclean = {
-    onLoad: function() {
-	this.strings = document.getElementById("keepitclean-strings");
-	dump ("keepitclean loaded\n");
-    },
+var keepitclean = {};
 
-    toolbar_button_validate: function(e) {
-	keepitclean.do_validate ();
-    }
+keepitclean.toolbar_button_validate = function(e) {
+    keepitclean.do_validate ();
+}
+
+keepitclean.onFirefoxLoad = function(event) {
+    dump ("onFirefoxLoad()\n");
+
+    // Find the most recently used window
+    var mediator = Components.classes['@mozilla.org/appshell/window-mediator;1']
+        .getService(Components.interfaces.nsIWindowMediator);
+    var doc = mediator.getMostRecentWindow("navigator:browser").document;
+
+    // Get the add-on bar for that window
+    var addonBar = doc.getElementById("addon-bar");
+
+    // Construct the new toolbar item
+    var newItem = doc.createElement("toolbaritem");
+    var itemLabel = doc.createElement("label");
+    // Add the item to the toolbar and set its text label
+    newItem.appendChild(itemLabel);
+    addonBar.appendChild(newItem);
+
+    itemLabel.value = "Hello world";
 };
 
-window.addEventListener("load", function () { keepitclean.onLoad(); }, false);
-    
+window.addEventListener("load", function () { keepitclean.onFirefoxLoad(); }, false);
+
+gBrowser.addEventListener ("load",
+			   function (ev) {
+			       var doc = ev.originalTarget;
+
+			       if (! doc instanceof HTMLDocument)
+				   return;
+
+			       while (doc.defaultView.frameElement)
+				   doc = doc.defaultView.frameElement.ownerDocument;
+
+			       dump ("loaded " + doc + "\n");			       
+			   }, 
+			   true);
